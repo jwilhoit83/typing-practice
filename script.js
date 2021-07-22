54d38581b2280c44ef13aed07acdf3796765e5b3
Jerod Wilhoit <jwilhoit83@gmail.com>
2021-07-21 22:45:19 -0500

    subtle changes to style, added conditional for chart data in case local storage is empty

--- script.js
+++ script.js
@@ -25,11 +25,11 @@ const minAcc = document.getElementById('min-acc')
 
 document.documentElement.classList = localStorage.getItem('prevTheme')
   ? localStorage.getItem('prevTheme')
-  : 'default'
+  : 'two-buck-chuck'
 
 themeSelect.value = localStorage.getItem('prevTheme')
   ? localStorage.getItem('prevTheme')
-  : 'default'
+  : 'two-buck-chuck'
 
 const wordList =
   'the of to and a in is it you that he was for on are with as I his they be at one have this from or had by hot but some what there we can out other were all your when up use word how said an each she which do their time if will way about many then them would write like so these her long make thing see him two has look more day could go come did my sound no most number who over know water than call first people may down side been now find any new work part take get place made live where after back little only round man year came show every good me give our under name very through just form much great think say help low line before turn cause same mean differ move right boy old too does tell sentence set three want air well also play small end put home read hand port large spell add even land here must big high such follow act why ask men change went light kind off need house picture try us again animal point mother world near build self earth father head stand own page should country found answer school grow study still learn plant cover food sun four thought let keep eye never last door between city tree cross since hard start might story saw far sea draw left late run while press close night real life few stop open seem together next white children begin got walk example ease paper often always music those both mark book letter until mile river car feet care second group carry took rain eat room friend began idea fish mountain north once base hear horse cut sure watch color face wood main enough plain girl usual young ready above ever red list though feel talk bird soon body dog family direct pose leave song measure state product black short numeral class wind question happen complete ship area half rock order fire south problem piece told knew pass farm top whole king size heard best hour better true during hundred am remember step early hold west ground interest reach fast five sing listen six table travel less morning ten simple several vowel toward war lay against pattern slow center love person money serve appear road map science rule govern pull cold notice voice fall power town fine certain fly unit lead cry dark machine note wait plan figure star box noun field rest correct able pound done beauty drive stood contain front teach week final gave green oh quick develop sleep warm free minute strong special mind behind clear tail produce fact street inch lot nothing course stay wheel full force blue object decide surface deep moon island foot yet busy test record boat common gold possible plane age dry wonder laugh thousand ago ran check game shape yes hot miss brought heat snow bed bring sit perhaps fill east weight language among'.split(
@@ -49,6 +49,7 @@ let wpmArr = scoresArr.map(item => item.score)
 let accuracyArr = scoresArr.map(item => item.accuracy)
 
 // initial population of chart and stats
+
 createChart()
 updateStats()
 
@@ -208,15 +209,24 @@ function updateData() {
 }
 
 function updateStats() {
-  avgWPM.innerText = (wpmArr.map(Number).reduce((a, c) => (a += c)) / wpmArr.length).toFixed(0)
-  maxWPM.innerText = wpmArr.slice().sort((a, b) => b - a)[0]
-  minWPM.innerText = wpmArr.slice().sort((a, b) => a - b)[0]
-  avgAcc.innerText = (
+  avgWPM.innerText = wpmArr.length > 0 ? (wpmArr.map(Number).reduce((a, c) => (a += c)) / wpmArr.length).toFixed(0) : 0
+  maxWPM.innerText = wpmArr.length > 0 ? wpmArr.slice().sort((a, b) => b - a)[0] : 0
+  minWPM.innerText = wpmArr.length > 0 ? wpmArr.slice().sort((a, b) => a - b)[0] : 0
+  avgAcc.innerText = accuracyArr.length > 0 ? (
     accuracyArr.map(Number).reduce((a, c) => (a += c)) / accuracyArr.length
-  ).toFixed(0)
-  maxAcc.innerText = accuracyArr.slice().sort((a, b) => b - a)[0]
-  minAcc.innerText = accuracyArr.slice().sort((a, b) => a - b)[0]
+  ).toFixed(0) : 0
+  maxAcc.innerText = accuracyArr.length > 0 ? accuracyArr.slice().sort((a, b) => b - a)[0] : 0
+  minAcc.innerText = accuracyArr.length > 0 ? accuracyArr.slice().sort((a, b) => a - b)[0] : 0
 }
+
+// function cursor(arr, idx) {
+//   if(arr[idx-1]){
+//     arr[idx-1].classList.remove('cursor')
+//   } else if(arr[idx+1]){
+//     arr[idx+1].classList.remove('cursor')
+//   }
+//   arr[idx].classList.add('cursor')
+// }
 
 function textListener() {
   const insertedText = text.value
@@ -226,9 +236,11 @@ function textListener() {
     for (let i = 0; i < insertedText.length; i++) {
       if (insertedText[i] === randomWords[index][i]) {
         letterArray[i].classList.add('success')
+        // cursor(letterArray, i)
       } else {
         letterArray[i].classList.add('warning')
         correctCount--
+        // cursor(letterArray, i)
       }
     }
   }
